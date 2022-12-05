56d897f4b7c6aa7cf468afc7269ace93123fc8c7
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-05 23:08:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 
-const element = <div>
-  <h1>hello react</h1>
-</div>
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode>
+  <TodoContainer />
+</React.StrictMode>,
+ document.getElementById('root'));
