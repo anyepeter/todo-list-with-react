80ed486ca46106903004af48c2b7a02a58151827
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-05 23:08:38 +0100

    Create TodoContainer.js

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,36 @@
+import React from "react"
+import TodosList from "./TodosList";
+
+class TodoContainer extends React.Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+  render() {
+    return (
+      <React.Fragment>
+        
+        <TodosList todos={this.state.todos} />
+
+      </React.Fragment>
+      
+    )
+  }
+}
+export default TodoContainer
